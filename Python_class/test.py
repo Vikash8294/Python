41e19d13c6df6542f8b2cd246f866199dd015f56
“Vikash8294” <vikashrahul147@gmail.com>
2024-11-24 22:24:40 +0530

    test

---
+++ Python_class/test.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    
+    result = add(5,3)
+    print(result)
+    
+# Example usage
+#result = add_two_numbers(5, 3)
+#print("The sum is:", result)
+
+#  Modify all your previous functions to print there execution time
