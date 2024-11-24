0e21a909060c345a013f5176332eb2d6c806402e
“Vikash8294” <vikashrahul147@gmail.com>
2024-11-24 22:18:57 +0530

    completed area

---
+++ MY EXPERIMENT/practiceQueeswtion/area.py
@@ -0,0 +1,19 @@
+
+#*****************AREA*********
+# s = float(input("enter the side of square :"))
+# Area = s*s
+# print(Area)
+
+#************average of floating number**********
+# num1 = float(input("enter the First number:"))
+# num2 = float(input("enter the second Number:"))
+# Avg = (num1+num2)/2
+# print("Average = ",Avg)
+    
+#***********************************************
+num1 = float(input("enter the First number:"))
+num2 = float(input("enter the second Number:"))
+if(num1>=num2):
+    print("True")
+else:
+    print("False")
