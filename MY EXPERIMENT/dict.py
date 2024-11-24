f6e9ff854047de3ae6a068c078257b78fa9bec39
“Vikash8294” <vikashrahul147@gmail.com>
2024-11-24 22:14:48 +0530

    completed dictonary

---
+++ MY EXPERIMENT/dict.py
@@ -0,0 +1,15 @@
+dict = {
+    "table" : ["a pice of furniture" , "list of fact figure"],
+    "cat" : "a small animal" 
+}
+print(type(dict))
+print(dict)
+#******************************
+
+# set1 = {"python", "java", "C++", "python", "javascript" }
+# set2 ={"java","python","java","C++","C"}
+# print(len(set1.union(set2)))
+
+# #or
+# set3 = {"python", "java", "C++", "python", "javascript","java","python","java","C++","C" }
+# print(len(set3))
