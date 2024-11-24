431fcd30f31ee44a87f3367186f33b173f3864ea
“Vikash8294” <vikashrahul147@gmail.com>
2024-11-24 22:19:11 +0530

    completed string

---
+++ MY EXPERIMENT/practiceQueeswtion/string.py
@@ -0,0 +1,31 @@
+# str1 = "apna college"
+# print(str1[1:4])# it will go from 1 to 3
+# print(str1[:4]) # from 0 compiler will understan
+# print(str1[5:]) # from 5 to its length compiler will understand
+
+
+#******************************************neagative index *************
+
+# str1= "Apple"
+# print(str1[-3:-1]) #pl
+# print(str1[-1:-3]) #pl
+
+# str1 ="apple"
+# str2 = "applii"
+# print(str1 < str2)
+# name = "vikash"
+# age = 21
+# print(f"my name is {name} and my age is {age}")
+# print("my name is",name ,"and my age is ",age)
+# print("my name is {} and my age is {}" .format(name,age))
+# print("my name is %s and my age is %d"%(name, age))
+# import sys
+# x = 1.002
+# size = sys.getsizeof(x)
+# print(size)
+# print(id(x))
+# print(round(x))
+
+scores = {"Alice": 10, "Bob": 15, "Charlie": 7}
+v = dict(sorted(scores.items()))
+print(v)
